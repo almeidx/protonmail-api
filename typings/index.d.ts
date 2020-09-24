eb03a4611f81857f32e2656e9a81b2640c490b28
almeidx <almeidx@pm.me>
2020-09-24 23:11:41 +0100

    added temp typings

---
+++ typings/index.d.ts
@@ -0,0 +1,22 @@
+/* Not the full module. Actually just what I need */
+declare module '@almeidx/protonmail-api' {
+  export interface ProtonClient {
+    sendMail(opts: SendMailOptions): Promise<ProtonClient>;
+    close(): void;
+  }
+
+  export interface SendMailOptions {
+    body: string;
+    subject: string;
+    to: string;
+  }
+
+  export interface ConnectOptions {
+    password: string;
+    username: string;
+  }
+
+  export default interface ProtonMail {
+    connect(opts: ConnectOptions): Promise<ProtonClient>;
+  }
+}
