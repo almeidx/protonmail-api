3610614c9cae2d749238df45132aca87e65c1beb
almeidx <almeidx@pm.me>
2020-09-30 20:37:35 +0100

    fix method name

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,7 @@
 /* Not the full module. Actually just what I need */
 declare module '@almeidx/protonmail-api' {
   export interface ProtonClient {
-    sendMail(opts: SendMailOptions): Promise<ProtonClient>;
+    sendEmail(opts: SendMailOptions): Promise<ProtonClient>;
     close(): void;
   }
 
